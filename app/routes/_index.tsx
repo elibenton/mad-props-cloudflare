289c5bda78ac77f2d31cabb22c6043db11154c45
Eli <eliunited@gmail.com>
2024-09-30 17:57:29 -0700

    intro text

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -200,7 +200,29 @@ export default function Index() {
 
 	return (
 		<div className='container mx-auto p-4'>
-			<h1 className='text-4xl font-bold mb-8'>Mad Props 2024</h1>
+			<h1 className='text-4xl font-bold mb-4 sticky top-0 bg-white border-b-2 -mx-4 px-4 pt-2 pb-4 border-black'>
+				MAD PROPS '24
+			</h1>
+
+			<p className='text-3xl font-serif italic p-4 '>
+				A non-partisan, not-too-serious voter guide to the California ballot measures.
+			</p>
+			<p className='text-sm px-4 py-1'>
+				Every two years, Californians get the distinct pleasure of voting on ballot
+				propositions. 26 states have them, but nowhere are they more celebrated or more reviled
+				than in the Golden State.
+			</p>
+			<p className='text-sm px-4 py-1'>
+				Propositions can often seem ludicrous (remember the condom prop in 2016?). They can
+				often feel antiquated (apparently slavery is on the ballot this year?). And they often
+				are written to be as confusing as possible.
+			</p>
+			<p className='text-sm px-4 py-1'>
+				This year there are 10 statewide propositions for your voting pleasure, and local
+				propositions in most major cities across California. They run the gamut: schools,
+				climate change, affordable housing, prescription drugs, factory farming, and more!
+			</p>
+			<p className='text-sm px-4 py-1 mb-12'>Let's dive in!</p>
 
 			<section className='mb-36'>
 				<h2 className='text-2xl font-extrabold sticky top-0 sm:relative bg-white border-b-2 border-black -mx-4 px-4 py-2 z-10 f'>
